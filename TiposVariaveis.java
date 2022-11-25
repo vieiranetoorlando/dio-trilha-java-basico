010a9c397cfdd4a71a364d325606c88cbdd9ff1b
ORLANDO VIEIRA NETO <110175293+vieiranetoorlando@users.noreply.github.com>
2022-11-25 13:43:37 -0300

    Adicionando Operadores

--- TiposVariaveis.java
+++ TiposVariaveis.java
@@ -1,3 +1,34 @@
 public class TiposVariaveis {
 
+    public static void main(String[] args) {
+
+        byte a = -128;
+        byte b = 127;
+
+        short c = -32768;
+        short d = 3277;
+
+        int e = -2147483648;
+        int f = 2147483647;
+
+        //Perceba a letra L no final do número
+        long g = -9223372036854775808L;
+        long h = 9223372036854775807L;
+
+        //Perceba a letra f no final do número
+        float i = -100.4345f;
+        float j = 123243.4345f;
+
+        double k = -3123.434354;
+        double l = 321321.3123435;
+
+        boolean m = false;
+        boolean n = true;
+
+        char o = 'a';
+        char p = '4';
+        char q = '?';
+
+    }
+
 }
